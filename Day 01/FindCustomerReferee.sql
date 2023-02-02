605066d70cb198c5f39014e5870565e387c2a3a8
Shivprasad Nikam <116971586+shivprasadnikam@users.noreply.github.com>
2023-02-02 13:11:04 +0530

    Create FindCustomerReferee.sql

---
+++ Day 01/FindCustomerReferee.sql
@@ -0,0 +1,8 @@
+
+                                                            // Find Customer Referee  //
+
+
+# Write your MySQL query statement below
+select name from
+customer
+where referee_id <> 2 or referee_id is null;
