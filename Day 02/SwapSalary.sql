8399ff9cb0a9991de8a95d43d80fc72a6eddc535
Shivprasad Nikam <116971586+shivprasadnikam@users.noreply.github.com>
2023-02-03 15:59:54 +0530

    Create SwapSalary.sql

---
+++ Day 02/SwapSalary.sql
@@ -0,0 +1,13 @@
+
+
+
+                                                                // Swap Salary  //
+                                                                
+                                                                
+# Write your MySQL query statement below
+update Salary
+set sex=case
+when sex='m' then 'f'
+when sex='f' then 'm'
+end;
+
